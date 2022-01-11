d0722020e3f2b9f30d9699c98e86a7a54c4a7e1d
Giorgio <gio.ricci@gmail.com>
2022-01-11 21:21:40 +0100

    Aggiorna main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,29 @@
 input.onButtonPressed(Button.A, function () {
     giocatore.move(-1)
-    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Sixteenth))
 })
 input.onButtonPressed(Button.B, function () {
     giocatore.move(1)
-    music.playTone(262, music.beat(BeatFraction.Whole))
+    music.playTone(262, music.beat(BeatFraction.Sixteenth))
 })
 let giocatore: game.LedSprite = null
-music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.Once)
+music.startMelody(music.builtInMelody(Melodies.Entertainer), MelodyOptions.OnceInBackground)
 game.setScore(0)
 let porta = game.createSprite(randint(0, 4), 0)
 giocatore = game.createSprite(randint(0, 4), 4)
 basic.forever(function () {
+    music.setVolume(20)
     for (let index = 0; index < 5; index++) {
-        basic.pause(500)
+        basic.pause(300)
         giocatore.change(LedSpriteProperty.Y, -1)
     }
     if (giocatore.isTouching(porta)) {
+        music.playTone(988, music.beat(BeatFraction.Eighth))
         game.addScore(1)
         giocatore = game.createSprite(randint(0, 4), 4)
     } else {
         giocatore.delete()
+        music.playTone(131, music.beat(BeatFraction.Eighth))
         basic.showIcon(IconNames.No)
         giocatore = game.createSprite(randint(0, 4), 4)
     }
