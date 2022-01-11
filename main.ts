69201ee18e9e568e4a8f0e5853eb8dd8345903c6
Giorgio <gio.ricci@gmail.com>
2022-01-11 20:30:28 +0100

    File iniziali per il progetto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onGesture(Gesture.TiltRight, function () {
+	
+})
+input.onGesture(Gesture.TiltLeft, function () {
+	
+})
+game.setScore(0)
+let porta = game.createSprite(randint(0, 4), 0)
+let giocatore = 0
+basic.forever(function () {
+	
+})
